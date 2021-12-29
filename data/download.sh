188f837ae1f34e50b3975250c376f0c469ef7729
Ondrej Platek <ondrej.platek@seznam.cz>
2021-12-29 13:50:10 +0000

    conda environment

--- data/download.sh
+++ data/download.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
 # download evaluation restuls
 wget -O vcc2018_listening_test_scores.zip "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3257/vcc2018_listening_test_scores.zip?sequence=1&isAllowed=y" 
 unzip vcc2018_listening_test_scores.zip
-rm vcc2018_listening_test_scores.zip
+echo rm vcc2018_listening_test_scores.zip
 
 # download submitted_systems_converted_speech
 wget -O vcc2018_submitted_systems_converted_speech.tar.gz "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3061/vcc2018_submitted_systems_converted_speech.tar.gz?sequence=10&isAllowed=y" 
 tar zxvf vcc2018_submitted_systems_converted_speech.tar.gz
-rm vcc2018_submitted_systems_converted_speech.tar.gz
+echo rm vcc2018_submitted_systems_converted_speech.tar.gz
 
 # rename dir
 mkdir submit
